6ab3fb25e8c73ae5d9316a7acb0d96850feb6975
unknown <manssniperkatoto@gmail.com>
2026-08-11 10:20:29 +0300

    create table AssetCategory

---
+++ app/Filament/Resources/AssetCategories/Schemas/AssetCategoryForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\AssetCategories\Schemas;
+
+use Filament\Schemas\Schema;
+
+class AssetCategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
