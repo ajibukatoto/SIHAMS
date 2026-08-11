cc49ed9739d94a3119bfad7b0e682e0ce293faa1
unknown <manssniperkatoto@gmail.com>
2026-08-11 11:56:44 +0300

    create asset model

--- app/Filament/Resources/Assets/Tables/AssetsTable.php
+++ app/Filament/Resources/Assets/Tables/AssetsTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Assets\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 
 class AssetsTable
@@ -13,7 +15,25 @@ class AssetsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Asset Name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('code')
+                    ->label('Asset Code')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('description')
+                    ->label('Description')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('assetCategory.name')
+                    ->label('Asset Category')
+                    ->sortable()
+                    ->searchable(),
+                ToggleColumn::make('is_active')
+                    ->label('Is Active')
+                    ->sortable(),
             ])
             ->filters([
                 //
--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+        'asset_category_id',
+        'is_active',
+    ];
+
+    public function assetCategory()
+    {
+        return $this->belongsTo(AssetCategory::class);
+    }
 }
