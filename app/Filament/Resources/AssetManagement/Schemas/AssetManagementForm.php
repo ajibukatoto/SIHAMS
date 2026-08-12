318a21b6af1cbd1ff8ed7ca5d907208c77be8795
unknown <manssniperkatoto@gmail.com>
2026-08-12 15:15:26 +0300

    creating  asset management resource

---
+++ app/Filament/Resources/AssetManagement/Schemas/AssetManagementForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\AssetManagement\Schemas;
+
+use Filament\Schemas\Schema;
+
+class AssetManagementForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
