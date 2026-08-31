f6900c2bbff0f90e3b6861bccac26ad486347d0a
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-08-31 13:58:08 +0300

    add redirect on asset maintenance resource

--- app/Filament/Resources/AssetMaintenances/Pages/CreateAssetMaintenance.php
+++ app/Filament/Resources/AssetMaintenances/Pages/CreateAssetMaintenance.php
@@ -4,8 +4,14 @@ namespace App\Filament\Resources\AssetMaintenances\Pages;
 
 use App\Filament\Resources\AssetMaintenances\AssetMaintenanceResource;
 use Filament\Resources\Pages\CreateRecord;
+use Override;
 
 class CreateAssetMaintenance extends CreateRecord
 {
     protected static string $resource = AssetMaintenanceResource::class;
+    #[Override]
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
