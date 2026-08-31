7636b379563d79b2a167562c6d46b30fa10edda1
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-08-31 14:17:00 +0300

    make redict page

--- app/Filament/Resources/Offices/Pages/EditOffice.php
+++ app/Filament/Resources/Offices/Pages/EditOffice.php
@@ -9,6 +9,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditOffice extends EditRecord
 {
     protected static string $resource = OfficeResource::class;
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
 
     protected function getHeaderActions(): array
     {
