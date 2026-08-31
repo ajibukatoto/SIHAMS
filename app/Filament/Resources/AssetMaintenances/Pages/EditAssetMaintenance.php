31a9120fc8f9d4407ad13b14e6dea522a1239e53
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-08-31 14:12:01 +0300

    make  redict on asset assignment

--- app/Filament/Resources/AssetMaintenances/Pages/EditAssetMaintenance.php
+++ app/Filament/Resources/AssetMaintenances/Pages/EditAssetMaintenance.php
@@ -9,6 +9,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditAssetMaintenance extends EditRecord
 {
     protected static string $resource = AssetMaintenanceResource::class;
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
 
     protected function getHeaderActions(): array
     {
