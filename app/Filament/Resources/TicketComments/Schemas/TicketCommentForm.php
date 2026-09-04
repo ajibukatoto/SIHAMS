25d367a7c425a5f9a2398c694ea01f6fe591b826
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-04 13:40:16 +0300

    create ticket comment resource

---
+++ app/Filament/Resources/TicketComments/Schemas/TicketCommentForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\TicketComments\Schemas;
+
+use Filament\Schemas\Schema;
+
+class TicketCommentForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
