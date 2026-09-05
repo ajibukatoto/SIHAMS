9e14db4cf788e460accb8fc905e8f5addf4ee09e
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-05 11:46:18 +0300

    insert role form

--- app/Filament/Resources/Roles/Schemas/RoleForm.php
+++ app/Filament/Resources/Roles/Schemas/RoleForm.php
@@ -2,7 +2,11 @@
 
 namespace App\Filament\Resources\Roles\Schemas;
 
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Spatie\Permission\Models\Permission;
 
 class RoleForm
 {
@@ -10,7 +14,28 @@ class RoleForm
     {
         return $schema
             ->components([
-                //
+                Section::make('Role Information')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Role Name')
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->maxLength(255),
+
+                        CheckboxList::make('permissions')
+                            ->label('Permissions')
+                            ->options(
+                                Permission::query()
+                                    ->orderBy('name')
+                                    ->pluck('name', 'id')
+                                    ->toArray()
+                            )
+                            ->searchable()
+                            ->bulkToggleable()
+                            ->columns(2)
+                            ->columnSpanFull(),
+                    ])
+                    ->columns(2),
             ]);
     }
 }
