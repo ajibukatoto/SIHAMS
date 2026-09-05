597ed47e26cc15b9a701af084e59fdcf03ce70e3
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-05 11:55:19 +0300

    create redirect on EditRole.php

--- app/Filament/Resources/Roles/Pages/EditRole.php
+++ app/Filament/Resources/Roles/Pages/EditRole.php
@@ -3,17 +3,14 @@
 namespace App\Filament\Resources\Roles\Pages;
 
 use App\Filament\Resources\Roles\RoleResource;
-use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRole extends EditRecord
 {
     protected static string $resource = RoleResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
