77781451905a9fad3837f833890ac8dbf11ecbb2
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-05 11:48:43 +0300

    insert role table

--- app/Filament/Resources/Roles/Tables/RolesTable.php
+++ app/Filament/Resources/Roles/Tables/RolesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Roles\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class RolesTable
@@ -13,7 +14,30 @@ class RolesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Role')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('guard_name')
+                    ->label('Guard')
+                    ->badge()
+                    ->sortable(),
+
+                TextColumn::make('permissions_count')
+                    ->label('Permissions')
+                    ->counts('permissions')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->dateTime()
+                    ->sortable(),
+
+                TextColumn::make('updated_at')
+                    ->label('Updated')
+                    ->dateTime()
+                    ->sortable(),
             ])
             ->filters([
                 //
@@ -25,6 +49,7 @@ class RolesTable
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 }
