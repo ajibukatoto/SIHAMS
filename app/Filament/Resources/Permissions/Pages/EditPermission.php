9f58d3b355540ba0bb1ff2bed1e2be55f0129d55
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-06 19:24:55 +0300

    update permision edit

--- app/Filament/Resources/Permissions/Pages/EditPermission.php
+++ app/Filament/Resources/Permissions/Pages/EditPermission.php
@@ -3,17 +3,14 @@
 namespace App\Filament\Resources\Permissions\Pages;
 
 use App\Filament\Resources\Permissions\PermissionResource;
-use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPermission extends EditRecord
 {
     protected static string $resource = PermissionResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
