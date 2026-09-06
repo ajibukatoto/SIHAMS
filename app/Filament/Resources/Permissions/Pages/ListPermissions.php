ab282216a76f6130862291fa73343e862130ed44
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-06 19:26:32 +0300

    update permision list

--- app/Filament/Resources/Permissions/Pages/ListPermissions.php
+++ app/Filament/Resources/Permissions/Pages/ListPermissions.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\Permissions\Pages;
 
 use App\Filament\Resources\Permissions\PermissionResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPermissions extends ListRecords
 {
     protected static string $resource = PermissionResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
