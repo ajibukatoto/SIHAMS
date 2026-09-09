684ff87f126318a296a3c7588bbc407a6a2b8ff4
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-09 14:45:16 +0300

    update Asset policy

--- app/Policies/AssetPolicy.php
+++ app/Policies/AssetPolicy.php
@@ -4,52 +4,51 @@ namespace App\Policies;
 
 use App\Models\Asset;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class AssetPolicy
 {
     /**
-     * Determine whether the user can view any models.
+     * Determine whether the user can view any assets.
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return $user->can('assets.view');
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine whether the user can view an asset.
      */
     public function view(User $user, Asset $asset): bool
     {
-        return false;
+        return $user->can('assets.view');
     }
 
     /**
-     * Determine whether the user can create models.
+     * Determine whether the user can create assets.
      */
     public function create(User $user): bool
     {
-        return false;
+        return $user->can('assets.create');
     }
 
     /**
-     * Determine whether the user can update the model.
+     * Determine whether the user can update an asset.
      */
     public function update(User $user, Asset $asset): bool
     {
-        return false;
+        return $user->can('assets.update');
     }
 
     /**
-     * Determine whether the user can delete the model.
+     * Determine whether the user can delete an asset.
      */
     public function delete(User $user, Asset $asset): bool
     {
-        return false;
+        return $user->can('assets.delete');
     }
 
     /**
-     * Determine whether the user can restore the model.
+     * Determine whether the user can restore an asset.
      */
     public function restore(User $user, Asset $asset): bool
     {
@@ -57,7 +56,7 @@ class AssetPolicy
     }
 
     /**
-     * Determine whether the user can permanently delete the model.
+     * Determine whether the user can permanently delete an asset.
      */
     public function forceDelete(User $user, Asset $asset): bool
     {
