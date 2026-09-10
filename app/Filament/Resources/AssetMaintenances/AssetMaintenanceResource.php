8239105b493c3ccdbffe2132dff4e8bdfafaa8eb
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-10 08:52:50 +0300

    update asset maintenance resource

--- app/Filament/Resources/AssetMaintenances/AssetMaintenanceResource.php
+++ app/Filament/Resources/AssetMaintenances/AssetMaintenanceResource.php
@@ -13,12 +13,14 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-
+use UnitEnum;
 class AssetMaintenanceResource extends Resource
 {
     protected static ?string $model = AssetMaintenance::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedWrenchScrewdriver;
+    protected static string|UnitEnum|null $navigationGroup = 'Asset Management';
+protected static ?int $navigationSort = 4;
 
     public static function form(Schema $schema): Schema
     {
