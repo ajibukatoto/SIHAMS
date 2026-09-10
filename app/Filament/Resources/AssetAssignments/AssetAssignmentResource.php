3d3545396dee81be0dedce2e46ac0c101e320d6b
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-10 08:50:30 +0300

    update asset assignment resource

--- app/Filament/Resources/AssetAssignments/AssetAssignmentResource.php
+++ app/Filament/Resources/AssetAssignments/AssetAssignmentResource.php
@@ -13,12 +13,14 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-
+use UnitEnum;
 class AssetAssignmentResource extends Resource
 {
     protected static ?string $model = AssetAssignment::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedClipboardDocumentCheck;
+    protected static string|UnitEnum|null $navigationGroup = 'Asset Management';
+protected static ?int $navigationSort = 3;
 
    //
 
