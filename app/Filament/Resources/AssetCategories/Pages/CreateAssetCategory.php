5fe72201da5fab13a101d6656d100f5bb871dd7e
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-14 12:03:28 +0300

    update create asset category

--- app/Filament/Resources/AssetCategories/Pages/CreateAssetCategory.php
+++ app/Filament/Resources/AssetCategories/Pages/CreateAssetCategory.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAssetCategory extends CreateRecord
 {
     protected static string $resource = AssetCategoryResource::class;
+      protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
+
