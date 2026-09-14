e3f3a9206c655638d03bf1fa38ed8e4c4803bf34
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-14 09:01:16 +0300

    update create ticket comment

--- app/Filament/Resources/TicketComments/Pages/CreateTicketComment.php
+++ app/Filament/Resources/TicketComments/Pages/CreateTicketComment.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\TicketComments\Pages;
 
 use App\Filament\Resources\TicketComments\TicketCommentResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateTicketComment extends CreateRecord
 {
     protected static string $resource = TicketCommentResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Automatically set the logged-in user as the comment author.
+        $data['user_id'] = Auth::id();
+
+        return $data;
+    }
 }
