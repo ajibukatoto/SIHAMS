445daf7b43c4e6b6ebe091a51b278c529e9661fb
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-14 08:55:38 +0300

    update ticket comment form

--- app/Filament/Resources/TicketComments/Schemas/TicketCommentForm.php
+++ app/Filament/Resources/TicketComments/Schemas/TicketCommentForm.php
@@ -17,17 +17,9 @@ class TicketCommentForm
                 Section::make('Ticket Comment')
                     ->description('Add a comment to a help desk ticket.')
                     ->schema([
-
                         Select::make('help_desk_ticket_id')
                             ->label('Help Desk Ticket')
                             ->relationship('ticket', 'ticket_number')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
-
-                        Select::make('user_id')
-                            ->label('Commented By')
-                            ->relationship('user', 'name')
                             ->searchable()
                             ->preload()
                             ->required(),
@@ -41,7 +33,7 @@ class TicketCommentForm
                         Toggle::make('is_internal')
                             ->label('Internal Comment')
                             ->helperText(
-                                'Enable this if the comment should be visible only to authorized ICT staff.'
+                                'Internal comments should only be visible to authorized ICT staff.'
                             )
                             ->default(false),
                     ])
