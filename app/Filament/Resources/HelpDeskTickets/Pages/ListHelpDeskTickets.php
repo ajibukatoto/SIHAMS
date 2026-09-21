303112982b2f9bedbfcfb1d14773e852705423ea
Ajibu Katoto <manssniperkatoto@gmail.com>
2026-09-21 12:40:11 +0300

    update help desk ticket form

--- app/Filament/Resources/HelpDeskTickets/Pages/ListHelpDeskTickets.php
+++ app/Filament/Resources/HelpDeskTickets/Pages/ListHelpDeskTickets.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\HelpDeskTickets\Pages;
 
 use App\Filament\Resources\HelpDeskTickets\HelpDeskTicketResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListHelpDeskTickets extends ListRecords
 {
     protected static string $resource = HelpDeskTicketResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label('Create Ticket')
+                ->icon('heroicon-o-plus'),
+        ];
+    }
 }
